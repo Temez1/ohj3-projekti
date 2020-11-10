5cfa58426fa9bb9422c1b208b42b451cdf2cbd77
JanHeikkila <jan.heikkila@tuni.fi>
2020-11-10 18:05:56 +0200

    assets for busses, teekkari, kioski and empty food holder

---
+++ teekkarinSekoiluSeikkailut/stopinserter.cpp
@@ -0,0 +1,8 @@
+#include "stopinserter.h"
+#include "Game.h"
+#include "Stop.h"
+
+stopInserter::stopInserter()
+{
+    auto name = new Stop
+}
---
+++ teekkarinSekoiluSeikkailut/stopinserter.h
@@ -0,0 +1,11 @@
+#ifndef STOPINSERTER_H
+#define STOPINSERTER_H
+
+
+class stopInserter
+{
+public:
+    stopInserter();
+};
+
+#endif // STOPINSERTER_H
